4b702e6b100e48b39e0b0fa3390f52d75d3c27af
David McGinnis <mcginnda@rose-hulman.edu>
2011-03-24 16:12:18 -0400

    PrimeNumberGen First Test Failed

---
+++ PrimeFactorization/src/PrimeNumberGen.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+
+public class PrimeNumberGen {
+
+	public static ArrayList<Integer> GeneratePrimeNumbers(int i)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ PrimeFactorization/src/TestPrimeNumberGen.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+
+
+public class TestPrimeNumberGen {
+	@Test
+	public void TestOne()
+	{
+		assertEquals(getList(), PrimeNumberGen.GeneratePrimeNumbers(1));
+	}
+	
+	private ArrayList<Integer> getList(int... vars)
+	{
+		ArrayList<Integer> returnable = new ArrayList<Integer>();
+		for(int i : vars)
+		{
+			returnable.add(i);
+		}
+		return returnable;
+	}
+}
