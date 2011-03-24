1182037ab68eda4c27b1020e21b45ed9353c5e28
David McGinnis <mcginnda@rose-hulman.edu>
2011-03-24 16:02:17 -0400

    Fourth test passed

--- PrimeFactorization/src/PrimeFactorization.java
+++ PrimeFactorization/src/PrimeFactorization.java
@@ -4,6 +4,12 @@ public class PrimeFactorization {
 
 	public static ArrayList<Integer> GetPrimeFactors(int num) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
+		if (num %2 == 0)
+		{
+			ret.add(2);
+			num /= 2;
+		}
+		
 		if (num > 1)
 		{
 			ret.add(num);
