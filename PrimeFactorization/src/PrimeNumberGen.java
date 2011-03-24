a1e425e48563f2381061949b769e14645b1fa552
David McGinnis <mcginnda@rose-hulman.edu>
2011-03-24 16:15:13 -0400

    PrimeNumberGen Third Test Passed

--- PrimeFactorization/src/PrimeNumberGen.java
+++ PrimeFactorization/src/PrimeNumberGen.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 
 public class PrimeNumberGen {
 
-	public static ArrayList<Integer> GeneratePrimeNumbers(int i)
+	public static ArrayList<Integer> GeneratePrimeNumbers(int num)
 	{
-		return new ArrayList<Integer>();
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		if (num > 2)
+		{
+			arr.add(2);
+		}
+		return arr;
 	}
-
 }
